12131336c30c1204af4304865b87970e46a3ec6f
K-U-R-C <jak.kurc@gmail.com>
2022-03-09 12:13:10 +0100

    Initial Commit

---
+++ JiPP/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  JiPP
+//
+//  Created by Jakub Kurc on 09/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
